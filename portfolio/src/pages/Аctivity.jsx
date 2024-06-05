91ea14ecb0ccf7cda63fdcb314930f47838ea25c
Ihar Skavysh <igogoblin@gmail.com>
2024-06-05 16:42:48 +0300

    feat: add github and codewars to activity

--- portfolio/src/pages/Аctivity.jsx
+++ portfolio/src/pages/Аctivity.jsx
@@ -1,7 +1,18 @@
+import gitStats from "../img/projects/gitHub.PNG";
+import codewarsStats from "../img/projects/Codewars.PNG";
+
 const Activity = () => {
   return (
-    <div>
-      <h1>Аctivity</h1>
+    <div className="activity">
+      <div className="activity__item">
+        <h2 className="title-2">GitHub</h2>
+        <img src={gitStats} alt="gitHub" />
+      </div>
+      <br />
+      <div className="activity__item">
+        <h2 className="title-2">Codewars</h2>
+        <img src={codewarsStats} alt="codewars" />
+      </div>
     </div>
   );
 };
